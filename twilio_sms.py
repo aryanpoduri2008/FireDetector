c45d539c1bb33a4916c43a7b2b17e4833dbb1d8f
ap <aryan.poduri@gmail.com>
2025-04-05 12:11:34 -0700

    fixed twilio messaging with whatsapp

--- twilio_sms.py
+++ twilio_sms.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
-account_sid = ''
-auth_token = ''
-twilio_phone_number = ''
-your_phone_number = ''  # Your personal phone number
+account_sid = 'AC9b6db539daaf8adce9a1b248b8f6b97e'
+auth_token = '27eb1135c85b971391b8f4dbef221b98'
+twilio_phone_number = 'whatsapp:+14155238886'
+your_phone_number = 'whatsapp:+16506276216'  # Your personal phone number
 
 client = Client(account_sid, auth_token)
 
@@ -12,7 +12,7 @@ def trigger_emergency_call():
     call = client.calls.create(
         to=your_phone_number,
         from_=twilio_phone_number,
-        url="http://demo.twilio.com/docs/voice.xml"  # Sample voice message URL
+        url="https://handler.twilio.com/twiml/EHa01dfc4b7041f5d2145e7842c25424db"  # Sample voice message URL
     )
     print(f"Emergency call initiated: {call.sid}")
 
